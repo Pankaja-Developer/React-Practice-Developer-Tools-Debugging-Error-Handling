43372161bca5c5117cd47f26d2e96a7d7038edd9
Pankaja-Developer <pankajasahu04@gmail.com>
2024-04-27 21:14:38 +0530

    literals used

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Expressions in JSX
+# Template Literals in JSX
 
-this code is used to how we can use expression.
+this code is used to how we can use Template Literals in JSX.
 
 we basically use html within javascript
 and again need to use javascript within html.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Pankaja Sahu";
+const fname = "Pankaja";
+const lname = "Sahu";
 ReactDOM.render(
   <>
-    <h1>My name is {name}</h1>
-    <p>{2 + 3}</p>
-    <p>{Math.random()}</p>
+    {/* <h1>My name is {`${fname} ${lname}`}</h1> */}
+    <h1>{`My name is ${fname} ${lname}`}</h1>
+    <p>{`${2 + 3}`}</p>
+    <p>{`${Math.random()}`}</p>
   </>,
   document.getElementById("root")
 );
