de60f20d88a979544b7c633ea98144b684d91e4f
Pankaja-Developer <pankajasahu04@gmail.com>
2024-04-29 20:22:55 +0530

    new feature time added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 
 const name = "Pankaj Sahu";
 const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
 
 ReactDOM.render(
   <>
     <h1>Hello My Name is {name}</h1>
-    <p>Current Date is = {currDate} </p>
+    <p>
+      Current Date is = {currDate} {currTime}
+    </p>
   </>,
   document.getElementById("root")
 );
