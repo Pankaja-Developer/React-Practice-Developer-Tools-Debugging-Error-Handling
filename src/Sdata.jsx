18ff4f91e6bed870b53da5424781c493bd4a9821
Pankaja-Developer <pankajasahu04@gmail.com>
2024-09-08 17:56:46 +0530

    file added

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+  {
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    sname: "DARK",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    sname: "DARK",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    sname: "DARK",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    sname: "DARK",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    sname: "DARK",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Card";
-
+import Sdata from "./Sdata";
+import "./index.css";
+console.log(Sdata[4]);
 ReactDOM.render(
   <>
+    <h1 className="heading_style">List Of 5 Netflix Series</h1>
     <Card
-      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
-      title="A Netflix Orignal Series"
-      sname="DARK"
-      link="https://www.netflix.com/in/title/80990668?source=35"
+      imgsrc={Sdata[0].imgscr}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
     />
     <Card
-      imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
-      sname="Stranger Things"
-      title="Netflix Original Series"
-      link="https://www.netflix.com/in/title/80990668?source=35"
+      imgsrc={Sdata[1].imgscr}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      link={Sdata[1].link}
+    />
+    <Card
+      imgsrc={Sdata[2].imgscr}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      link={Sdata[2].link}
+    />
+    <Card
+      imgsrc={Sdata[3].imgscr}
+      title={Sdata[3].title}
+      sname={Sdata[3].sname}
+      link={Sdata[3].link}
+    />
+    <Card
+      imgsrc={Sdata[4].imgscr}
+      title={Sdata[4].title}
+      sname={Sdata[4].sname}
+      link={Sdata[4].link}
     />
   </>,
   document.getElementById("root")
