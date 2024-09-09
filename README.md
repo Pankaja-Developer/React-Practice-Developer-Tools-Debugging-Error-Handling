62899541f4a1ee0403d4b40fafaf04cbf9f15789
Pankaja-Developer <pankajasahu04@gmail.com>
2024-09-09 17:33:09 +0530

    files added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Props
+# Props using map aad fat arrow method
 
-How to make custom properties within components
+How to make custom properties within components in different way.
--- src/index.js
+++ src/index.js
@@ -3,40 +3,44 @@ import ReactDOM from "react-dom";
 import Card from "./Card";
 import Sdata from "./Sdata";
 import "./index.css";
-console.log(Sdata[4]);
+
+// function ncard(value) {
+//   return (
+//     <Card
+//       imgsrc={value.imgscr}
+//       title={value.title}
+//       sname={value.sname}
+//       link={value.link}
+//     />
+//   );
+// }
+
 ReactDOM.render(
   <>
-    <h1 className="heading_style">List Of 5 Netflix Series</h1>
-    <Card
-      imgsrc={Sdata[0].imgscr}
-      title={Sdata[0].title}
-      sname={Sdata[0].sname}
-      link={Sdata[0].link}
-    />
-    <Card
-      imgsrc={Sdata[1].imgscr}
-      title={Sdata[1].title}
-      sname={Sdata[1].sname}
-      link={Sdata[1].link}
-    />
-    <Card
-      imgsrc={Sdata[2].imgscr}
-      title={Sdata[2].title}
-      sname={Sdata[2].sname}
-      link={Sdata[2].link}
-    />
-    <Card
-      imgsrc={Sdata[3].imgscr}
-      title={Sdata[3].title}
-      sname={Sdata[3].sname}
-      link={Sdata[3].link}
-    />
-    <Card
-      imgsrc={Sdata[4].imgscr}
-      title={Sdata[4].title}
-      sname={Sdata[4].sname}
-      link={Sdata[4].link}
-    />
+    <h1 className="heading_style">List Of 5 Netflix Seriesvalue </h1>
+    {/* {Sdata.map(ncard)} */}
+
+    {/* {Sdata.map(function ncard(value) {
+      return (
+        <Card
+          imgsrc={value.imgscr}
+          title={value.title}
+          sname={value.sname}
+          link={value.link}
+        />
+      );
+    })} */}
+
+    {Sdata.map((value) => {
+      return (
+        <Card
+          imgsrc={value.imgscr}
+          title={value.title}
+          sname={value.sname}
+          link={value.link}
+        />
+      );
+    })}
   </>,
   document.getElementById("root")
 );
